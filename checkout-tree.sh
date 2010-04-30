cd6f3c5c69a88d373ee015b9df5c66696434c051
Hannes Magnusson <bjori@php.net>
2010-04-30 18:49:03 +0200

    Add support for more then just php-src

--- checkout-tree.sh
+++ checkout-tree.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-REPOSITORY="git://github.com/php/php-src.git"
+GIT_ROOT_REPOSITORY="git://github.com/php"
+SVN_ROOT_REPOSITORY="http://svn.php.net/repository"
+GIT_REPOSITORY="php-src.git"
+SVN_REPOSITORY="php/php-src/"
+
 
 usage() {
     cat <<EOF
-checkout-tree <directory> <repository-url>
+$0 <directory> <git-repository-name> <svn-repository-name>
 
-Checkout the PHP sourcecode from Github and reinitialize git-svn.
+Checkout a module from svn.php.net via Github and reinitialize git-svn.
 Run this script to get a valid git-svn repository that can be used to commit
 back to svn.php.net.
 EOF
 }
 
-if test $# -eq 2
+if test $# -eq 3
 then
-    REPOSITORY=$2
+  GIT_REPOSITORY=$2
+  SVN_REPOSITORY=$3
+elif test $# -eq 2
+then
+    GIT_REPOSITORY=$2
 elif test $# -ne 1
 then
     usage;
@@ -23,9 +31,9 @@ fi
 
 DIR=$1
 
-echo "Repository: $REPOSITORY"
+echo "Repository: $GIT_REPOSITORY"
 echo "Directroy:  $DIR"
 echo "Initialize..."
 
-(mkdir $DIR && cd $DIR && git init &&git remote add origin $REPOSITORY && git config remote.origin.fetch 'refs/remotes/*:refs/remotes/*' && git fetch &&
-git checkout -b master trunk && git svn init -s http://svn.php.net/repository/php/php-src/ && git svn rebase)
+(echo "mkdir $DIR && cd $DIR && git init &&git remote add origin $GIT_ROOT_REPOSITORY/$GIT_REPOSITORY && git config remote.origin.fetch 'refs/remotes/*:refs/remotes/*' && git fetch && git checkout -b master trunk && git svn init -s $SVN_ROOT_REPOSITORY/$SVN_REPOSITORY && git svn rebase")
+
